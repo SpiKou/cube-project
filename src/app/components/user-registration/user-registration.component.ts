3c8485b1ae26cd14881760333236ab46110086d3
S. Koutelopoulos <spiros.koutelopoulos@gmail.com>
2024-06-18 01:40:34 +0300

    registration primer

--- src/app/components/user-registration/user-registration.component.ts
+++ src/app/components/user-registration/user-registration.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Form, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { UserService } from 'src/app/shared/services/user.service';
+import { first } from 'rxjs';
 
 
 @Component({
@@ -15,4 +16,26 @@ import { UserService } from 'src/app/shared/services/user.service';
 })
 export class UserRegistrationComponent {
   userService = inject(UserService);
+
+  form = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8)])
+  },
+  this.passwordConfirmValidator,
+);
+
+  passwordConfirmValidator(form: FormGroup) {
+    if (form.get('password').value !==form.get('confirmPassword').value) {
+      form.get('confirmPassword').setErrors({passwordMismatch: true});
+      return {passwordMismatch: true};
+    }
+    return {}
+  }
+
+  onSubmit(value:any) {
+    console.log(value);
+  }
 }
