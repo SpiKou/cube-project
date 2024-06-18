0e180a93e6505a1b29dc31534758ca6016f7f8d4
S. Koutelopoulos <spiros.koutelopoulos@gmail.com>
2024-06-19 00:07:50 +0300

    update

--- src/app/components/user-registration/user-registration.component.ts
+++ src/app/components/user-registration/user-registration.component.ts
@@ -5,6 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { UserService } from 'src/app/shared/services/user.service';
 import { first } from 'rxjs';
+import { User } from 'src/app/shared/interfaces/user';
 
 
 @Component({
@@ -28,7 +29,7 @@ export class UserRegistrationComponent {
 );
 
   passwordConfirmValidator(form: FormGroup) {
-    if (form.get('password').value !==form.get('confirmPassword').value) {
+    if (form.get('confirmPassword').value !==form.get('confirmPassword').value) {
       form.get('confirmPassword').setErrors({passwordMismatch: true});
       return {passwordMismatch: true};
     }
@@ -37,5 +38,18 @@ export class UserRegistrationComponent {
 
   onSubmit(value:any) {
     console.log(value);
+
+    const user = this.form.value as User;
+    delete user['confirmPassword'];
+
+    this.userService.registerUser(user).subscribe({
+      next: (response) => {
+        console.log('User registered', response.msg);
+      },
+      error: (response) => {
+        const message = response.error.msg;
+        console.log('Error registering user', message);
+      },
+    });
   }
 }
