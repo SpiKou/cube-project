bafc7b0765c96d9e6e18872d5fc6d0962dcffafb
S. Koutelopoulos <spiros.koutelopoulos@gmail.com>
2024-06-29 11:34:49 +0300

    user registration form tweaks

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiURL: 'https://github.com/SpiKou/SpiKou.github.io', // https://spiKou.github.io/cube-project/
+    apiURL: 'http://localhost:3000', // https://spiKou.github.io/cube-project/
 };
