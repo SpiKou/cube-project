0e7b3a041991d24bae7525951db4b8662acd8f6e
S. Koutelopoulos <spiros.koutelopoulos@gmail.com>
2024-07-06 17:14:35 +0300

    removing flask backend

--- Back_End/finalProject/clients/token-auth.py
+++ Back_End/finalProject/clients/token-auth.py
@@ -1,8 +1,11 @@
 import requests
 
 def client():
-    credentials = {"username":"admin", "password":"1234"}
-    response = requests.post('http://127.0.0.1:8000/dj-rest-auth/login/', data=credentials)
+    # credentials = {"username":"admin", "password":"1234"}
+    # response = requests.post('http://127.0.0.1:8000/dj-rest-auth/login/', data=credentials)
+    access_token = 'Token 5d57edbb4536ba4b2b4dc6b4d179fd2ce067948c'
+    headers = {"Authorization": access_token}
+    response = requests.get("http://127.0.0.1:8000/user/users/", headers=headers)
     print('Status code:', response.status_code)
     response_data = response.json()
     print(response_data)
